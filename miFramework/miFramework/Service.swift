f599132c1b31c80758f68a0a529778bc869468fc
PedroMayo <pedro@gmail.com>
2019-07-26 11:20:15 -0500

    Terminado el framework en xcode

---
+++ miFramework/miFramework/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  miFramework
+//
+//  Created by pedro mayo on 26/07/19.
+//  Copyright © 2019 pedro mayo. All rights reserved.
+//
+
+import Foundation
+public class Service{
+    private init(){} //esto hace q el constructor sea privado
+    public static func hacerAlgo()->String{
+        return "ya hizo algo, fin de la funcion"
+    }
+}
---
+++ miFramework/miFramework/miFramework.h
@@ -0,0 +1,19 @@
+//
+//  miFramework.h
+//  miFramework
+//
+//  Created by pedro mayo on 26/07/19.
+//  Copyright © 2019 pedro mayo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for miFramework.
+FOUNDATION_EXPORT double miFrameworkVersionNumber;
+
+//! Project version string for miFramework.
+FOUNDATION_EXPORT const unsigned char miFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <miFramework/PublicHeader.h>
+
+
